8cc9c79d8a7a62b551ed37687fe27e83faa0c2ae
Johnclan <40492133+Johnclan@users.noreply.github.com>
2023-05-03 13:38:02 +0800

    Delete 神经网络训练1 directory

--- 神经网络训练1/dydx.m
+++
@@ -1,3 +0,0 @@
-function dy = dydx(~,y,y_d,C,loss)
-BRS = [zeros(1,1) zeros(1,5)+4.5*10^(-8) zeros(1,30)]';
-dy = ((C*(y+y_d)'-loss').*y'+ BRS.*y_d')';
--- 神经网络训练1/findplan.m
+++
@@ -1,7 +0,0 @@
-for i = 1:5000
-    if B(i)>=15&&WDG(i)<=2
-        disp(i)
-        disp(WDG(i))
-        disp(B(i))
-    end
-end
